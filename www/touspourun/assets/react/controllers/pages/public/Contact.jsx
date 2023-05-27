a7324089df7250283e4f8e8b21fd4aef324c7184
sorasarah <ouriachi.sarah@gmail.com>
2023-05-27 20:22:44 +0200

    test

--- www/touspourun/assets/react/controllers/pages/public/Contact.jsx
+++ www/touspourun/assets/react/controllers/pages/public/Contact.jsx
@@ -1,20 +1,9 @@
-import React, { useState }  from 'react';
-import { Button, Card, CardContent, Grid, TextField, Typography } from "@mui/material";
-import { validEmail } from "../../_utils/Regex";
-import "../../../../styles/contact.css"
+import React from 'react';
 
 export const Contact = () => {
-    const [email, setEmail] = useState('');
-    const [emailErr, setEmailErr] = useState(false);
-    const validate = () => {
-     if (!validEmail.test(email)) {
-        setEmailErr(true);
-     }
-    };
-
     return (
-        <div className='containerContact'>
-            <h1>page contact</h1>
+        <div>
+            <h1>je suis une page contact</h1>
         </div>
     );
 };
