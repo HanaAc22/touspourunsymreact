d243265738141edf2184324b3035ef58e5b87250
sorasarah <ouriachi.sarah@gmail.com>
2023-06-02 12:11:43 +0200

    desactivation reges login

--- www/touspourun/assets/react/controllers/_utils/Regex.jsx
+++ www/touspourun/assets/react/controllers/_utils/Regex.jsx
@@ -1,12 +1,15 @@
 export const validEmail = new RegExp(
-  "[a-zA-Z0-9-]+@[a-zA-Z0-9-]+.[a-zA-Z0-9-]{2,6}$"
+  "[a-zA-Z0-9-_]+@[a-zA-Z0-9-_]+.[a-zA-Z0-9-]{2,6}$"
 );
 export const validPassword = new RegExp(
   "^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{8,}$"
 );
-export const validSiret = new RegExp("[0-9-]{14}$");
+export const validConnectPassword = new RegExp(
+  "^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{8,}$"
+);
+export const validSiret = new RegExp("[0-9]{14}$");
 export const validNameRgex = new RegExp("^[a-zA-Z0-9-]{2,25}$");
-export const validMsgRgex = new RegExp("^[a-zA-Z0-9-_ '.#?!@$%^&*-,\n\r\s\b]{5,550}$");
+export const validMsgRgex = new RegExp("^[a-zA-Z0-9-_ '.#?!@$%^&*,\n\r\s\b]{5,550}$");
 
 //Au moins un chiffre. Vous pouvez supprimer cette condition en supprimant (?=.* ?[0-9])
 //Au moins un caractère spécial, Vous pouvez supprimer cette condition en supprimant (?=.* ?[#?!@$%^&*-])
